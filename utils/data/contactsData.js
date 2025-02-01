37aa3cc3c964a158c9643ed94a53cb2826d3dbfc
Aaryan meena <134821046+aaryan359@users.noreply.github.com>
2025-02-01 15:51:10 +0530

    portfolo

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'aaryanmeena96@gmail.com',
+    phone: '+91-9799819141',
+    address: 'Gwalior,Madhya Pradesh,474015 ',
+    github: 'https://github.com/aaryan359',
+    facebook: 'https://www.linkedin.com/in/aryan-meena-899b80301/',
+    linkedIn: 'https://www.linkedin.com/in/aryan-meena-899b80301/',
+    twitter: 'https://twitter.com/',
+    stackOverflow: '',
+    devUsername: "Aryan"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "ARYAN MEENA",
+  profile: '/profile.jpg',
+  designation: "Software Developer",
+  description: "My name is ARYAN MEENA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'aaryanmeena96@gmail.com',
+  phone: '+91-9799819141',
+  address: 'Gwalior ,Madhya Pradesh,474015 ',
+  github: 'https://github.com/aaryan359',
+  facebook: 'https://www.linkedin.com/in/aryan-meena-899b80301/',
+  linkedIn: 'https://www.linkedin.com/in/aryan-meena-899b80301/',
+  twitter: 'https://twitter.com/',
+  stackOverflow: '',
+  leetcode: "https://leetcode.com",
+  devUsername: "aryan",
+  resume: "https://drive.google.com/file/d/1tN46vwWTcWZw5PA96jIKGVW-rgxgHedt/view?usp=drive_link"
+}
