7d68ea40badfdae9b8ff46589581511484368d6c
Aaryan meena <134821046+aaryan359@users.noreply.github.com>
2025-02-14 23:58:04 +0530

    faf

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
           <Link
             href="/"
             className=" text-[#16f2b3] text-3xl font-bold">
-           ARYAN MEENA
+            ARYAN MEENA
           </Link>
         </div>
 
@@ -20,6 +20,11 @@ function Navbar() {
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
+
+
+          {/* <li>
+
+          </li> */}
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
           </li>
