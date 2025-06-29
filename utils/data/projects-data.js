042e28aa952c63895cfd8fb80c469bc87d320856
aaryan359 <aaryanmeena96@gmail.com>
2025-06-30 01:49:15 +0530

    new project added

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -42,6 +42,7 @@ export const projects = [
 		liveUrl: "hhttps://drive.google.com/file/d/1WuC29NH3BPawQrvmq9vHsuShRHj5RxJ-/view",
 		githubUrl: "https://github.com/aaryan359/EveryPenny-1",
 	},
+
 	{
 		id: 3,
 		name: "Vehical Rental App",
@@ -68,8 +69,44 @@ export const projects = [
 
 		],
 		liveUrl: "https://drive.google.com/file/d/1IvF-0Cptd6FGrzVFFltJM4fRZiqYPKij/view",
-		githubUrl: "https://github.com/aaryan359/EveryPenny-1",
+		githubUrl: "https://github.com/aaryan359/GoAmigo-Admin-Frontend",
 	},
+
+	{
+		id: 4,
+		name: "PromptX",
+		description:
+			"Market Place for AI prompts(system and user prompts) And chat bot with dedicated button for specific tasks.",
+		technologies: [
+			"React Native",
+			"Express",
+			"Nodejs",
+			"MongoDB",
+			"Typescript",
+		],
+		images: [
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219792.png?updatedAt=1751227865413',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219783.png?updatedAt=1751227863497',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219879.png?updatedAt=1751227865815',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219821.png?updatedAt=1751227872546',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219909.png?updatedAt=1751227884738',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219929.png?updatedAt=1751227883795',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219942.png?updatedAt=1751227880693',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219936.png?updatedAt=1751227878779',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219904.png?updatedAt=1751227877873',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219869.png?updatedAt=1751227867512',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219848.png?updatedAt=1751227862689',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219798.png?updatedAt=1751227862436',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot_1751219855.png?updatedAt=1751227861938',
+				'https://ik.imagekit.io/moq2nlye8/Portfolio-project-images/PromptX/Screenshot%20from%202025-06-30%2001-44-49.png?updatedAt=1751228228100'
+
+
+		],
+		liveUrl: "https://drive.google.com/file/d/1JPOGCUPsh-abcE7WhQtILiSD1r3z8aev/view?usp=sharing",
+		githubUrl: "https://github.com/aaryan359/PromptX",
+	},
+
+	  
 
 ];
 
