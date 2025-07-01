299bcaee7720830ffc0ffb96f6ae6b771d9c0506
aaryan359 <aaryanmeena96@gmail.com>
2025-07-01 10:16:49 +0530

    FIX: send email button css not working

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -87,10 +87,19 @@ export default function Contact() {
       className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 text-sm md:text-base"
     />
     <button
-      className="flex items-center justify-center absolute right-1 top-1 px-2 md:px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-20 md:w-28 text-sm md:text-base"
-      type="submit">
-      {form.state === "Sending" ? <LoadingSpinner /> : "Send"}
-    </button>
+  className="flex items-center justify-center absolute right-1 top-1 px-2 md:px-4 font-bold h-8 bg-green-400 dark:bg-green-400 text-gray-900  rounded w-20 md:w-28 text-sm md:text-base"
+  type="submit"
+  disabled={form.state === "loading"}
+>
+  {form.state === "loading" ? (
+    <>
+      <LoadingSpinner />
+      <span className="ml-2">Sending...</span>
+    </>
+  ) : (
+    "Send"
+  )}
+</button>
   </form>
 
   {/* Message states remain same */}
