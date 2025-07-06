c2f1d13a282963840f3ac31bd0a260836a76b7ac
aaryan359 <aaryanmeena96@gmail.com>
2025-07-06 23:59:50 +0530

    new update

--- utils/data/upcomingProjects.js
+++ utils/data/upcomingProjects.js
@@ -15,6 +15,9 @@ export const upcomingProjects = [
   title: "StoryEd - AI-Powered Story Learning for Kids",
   description: "An educational platform that transforms textbook stories into engaging videos using AI-generated visuals and avatars, helping children from KG to 8th grade learn through storytelling."
  },
-
+ {
+    "title": "ReMarkket - Trusted Platform for Second-Hand Deals",
+    "description": "A secure marketplace for buying and selling second-hand items. Sellers can list products, and once a buyer shows interest, the seller is notified. Payment is collected from the buyer and held by the app until the item is delivered and confirmed as received by the buyer. Only then is the payment released to the seller, ensuring trust and reducing fraud."
+  }
 
 ];
